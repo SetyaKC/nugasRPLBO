94594e78015bb062dc658420e9b061107d36439c
anglnenads <93342734+anglnenads@users.noreply.github.com>
2023-05-13 16:54:56 +0700

    Delete Mbanking_Believe directory

--- Mbanking_Believe/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.mbanking_believe {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.mbanking_believe to javafx.fxml;
-    exports com.example.mbanking_believe;
-}
